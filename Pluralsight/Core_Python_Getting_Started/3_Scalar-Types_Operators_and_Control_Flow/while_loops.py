e0530160e2c34dd73febccae36bdfd58c5b1ebd5
aandr26 <aandr26@wgu.edu>
2020-04-08 12:54:11 -0600

    m

---
+++ Pluralsight/Core_Python_Getting_Started/3_Scalar-Types_Operators_and_Control_Flow/while_loops.py
@@ -0,0 +1,8 @@
+while True:
+    response = input()
+    if int(response) % 7 == 0:
+        print ("Hello, world")
+        break
+    else:
+        print ("You are awful")
+        break
---
+++ Pluralsight/Core_Python_Getting_Started/4_Introducing_Strings_Collections_and_Iteration/string_literals.py
@@ -0,0 +1,16 @@
+# Multiline strings
+""" This is
+a multiline
+string"""
+''' So
+is
+this. '''
+
+m = 'This string\nspans multiple\nlines'
+print(m)
+
+k = 'A \\ in a string'
+print(k)
+
+s = 'parrot'
+print(s[0])
