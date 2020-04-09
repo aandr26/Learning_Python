ce135c5b253464561465abad6ff52973d992d018
aandr26 <aandr26@wgu.edu>
2020-04-09 07:50:20 -0600

    m

--- Pluralsight/Core_Python_Getting_Started/5_modularity/words.py
+++ Pluralsight/Core_Python_Getting_Started/5_modularity/words.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python3
+"""Retrieve and print words from a URL.
+
+Usage:
+    
+    python3 words.py <URL>
+
+"""
+
+import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    
+    Returns:
+        A list of strings containing the words 
+        from the document.
+    """
     story = urlopen(url)
     story_words = []
     for line in story:
@@ -11,12 +30,22 @@ def fetch_words(url):
     return story_words
 
 def print_items(items):
+    """Print items one per line.
+
+       Args:
+           An iterable seies of printable items.
+    """
     for item in items:
         print(item)
 
 def main():
+    """Print each word from a text document from a URL.
+       
+       Args:
+           url: The URL of a UTF-8 text document.
+    """
     words = fetch_words()
     print_words(words)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1]) # The 0th arg is the module filename.
