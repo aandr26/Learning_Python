4d35ff92518a7c9f24ad99c792e1d56eec8cba42
aandr26 <aandr26@wgu.edu>
2020-04-16 08:57:24 -0600

    Comments, if statements, and message to canvas.

--- Coursera/Week3a/Exercises/circle_radius_interactive.py
+++ Coursera/Week3a/Exercises/circle_radius_interactive.py
@@ -2,8 +2,6 @@
 
 ###################################################
 # Student should add code where relevant to the following.
-
-
 import simplegui 
 
 # Define globals - Constants are capitalized in Python
@@ -14,21 +12,33 @@ ball_radius = 20
 
 # Draw handler
 def draw(canvas):
+    ''' This function prints the circle to canvas, 
+        and some text based on the value of ball radius '''
     global HEIGHT
-    global WIDTH 
+    global WIDTH
+    global ball_radius
     canvas.draw_circle([WIDTH/2, HEIGHT/2], ball_radius, 2, "Red")
-
+    if ball_radius <= 5:
+        canvas.draw_text("Cannot decrease size further", [WIDTH/4, (HEIGHT/2)+25], 20, "Blue")
+    
 # Event handlers for buttons
 def increase_radius():
+    ''' This function increases the radius of the circle '''
     global RADIUS_INCREMENT
     global ball_radius
     ball_radius += RADIUS_INCREMENT
     return ball_radius
 
+
 def decrease_radius():
+    ''' This function decreases the radius of the circle '''
     global RADIUS_INCREMENT
     global ball_radius
-    ball_radius -= RADIUS_INCREMENT
+    # If that prevents the value of ball_radius from becoming negative.
+    if ball_radius <= 5:
+        ball_radius == 5
+    else:
+        ball_radius -= RADIUS_INCREMENT
     return ball_radius
     
 # Create frame and assign callbacks to event handlers
@@ -40,4 +50,3 @@ frame.add_button("Decrease radius", decrease_radius, 100)
 
 # Start the frame animation
 frame.start()
-
